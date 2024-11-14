bad92ede18c67037f626f7601541f3dcd98a5b27
Carson Jennings <cjen02@gmail.com>
2024-11-14 15:20:15 -0800

    Added a POST user endpoint so that API can create new users using: first_name, last_name, password, and email

--- server/app.js
+++ server/app.js
@@ -12,11 +12,25 @@ app.listen(PORT, () => {
 
 app.get("/status", (request, response) => {
     const status = {
-        "Status" : "Running",
+        "status" : "Running",
     };
 
     response.send(status);
     console.log("GET /status request");
 });
 
-
+app.post("/user", (request, response) => {
+    const { first_name, last_name, password, email } = request.body;
+    const sql = `INSERT INTO users (first_name, last_name, password, email) VALUES (?, ?, ?, ?)`;
+    db.run(sql, [first_name, last_name, password, email], (err) => {
+        if (err) {
+            response.status(400).json({ error: err.message });
+            console.log("POST /users request failed");
+            return;
+        }
+        response.json({
+            "status" : "Success",
+            data : {first_name, last_name, email}
+        });
+    });
+});
