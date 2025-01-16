c051571ac8fb3235e781351ba3718c8cf34c8bef
Carson Jennings <cjen02@gmail.com>
2025-01-15 18:39:12 -0800

    Added some better stylying to the navbar

--- experimental/bracket-league/app/ui/navbar.tsx
+++ experimental/bracket-league/app/ui/navbar.tsx
@@ -4,12 +4,13 @@ import Link from 'next/link';
 
 export default function NavBar() {
     return (
-        <nav className="flex p-4 items-center border-b-gray-200 border-b-2">
+        <nav className="flex p-4 items-center bg-gradient-to-r from-red-400 to-blue-400">
             <div className="">
                 <ul className='flex items-center'>
                     <li className='mr-16'>
-                        <Link href='/' className='py-4'>
+                        <Link href='/'>
                             <Image
+                            className='rounded-xl'
                             src="/bracket-league.png"
                             width={60}
                             height={60}
@@ -18,17 +19,17 @@ export default function NavBar() {
                         </Link>
                     </li>
                     <li>
-                        <Link href="/brackets" className='px-4 py-4 hover:text-red-500 duration-150 text-base font-semibold'>
+                        <Link href="/brackets" className='px-4 py-4 text-white hover:text-red-100 duration-150 text-base font-semibold'>
                             Your Brackets
                         </Link>
                     </li>
                     <li>
-                        <Link href="/leagues" className='px-4 py-4 hover:text-red-500 duration-150 text-base font-semibold'>
+                        <Link href="/leagues" className='px-4 py-4 text-white hover:text-red-100 duration-150 text-base font-semibold'>
                             Your Leagues
                         </Link>
                     </li>
                     <li>
-                        <Link href="/create" className='px-4 py-4 hover:text-red-500 duration-150 text-base font-semibold'>
+                        <Link href="/create" className='px-4 py-4 text-white hover:text-red-100 duration-150 text-base font-semibold'>
                             Create
                         </Link>
                     </li>
