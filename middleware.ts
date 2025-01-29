4b6d0f85be6674dc180122d03eca15fb686c4466
Carson Jennings <cjen02@gmail.com>
2025-01-29 11:34:32 -0800

    Updated middleware.ts so that users are automatically redirected to the login when they try and access pages that require the user to be logged in

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,17 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@/app/lib/sessions";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const userSession = await updateSession(request);
+
+  // If session doesn't exist user is not logged in and will be redirected to login
+  if (!userSession) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return userSession;
+}
+
+export const config = {
+  matcher: '/dashboard/:path*',
 }
