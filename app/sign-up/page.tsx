97b200243a0e8cd66a8f613b1bbbd1d3c78aedbe
Carson Jennings <cjen02@gmail.com>
2025-02-08 13:58:35 -0800

    Added the navbar with the link to landing page and links to signup and login pages to the top of the sign up and login pages

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,8 +1,10 @@
 import SignUp from "@/app/ui/sign-up/sign-up";
+import LandingNavbar from "@/app/ui/landing-navbar";
 
 export default function Page() {
     return (
         <main className="flex flex-col items-center min-h-lvh">
+            <LandingNavbar />
             <SignUp />
         </main>
     );
