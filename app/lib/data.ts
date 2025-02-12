11fe9d803604c0088a4740cf7c8b9ac6b2173702
Carson Jennings <cjen02@gmail.com>
2025-02-12 14:27:56 -0800

    Added createBracket function so that brackets can be created and added to the database

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -78,11 +78,49 @@ export async function createLeague(user_id: bigint, name: string, description: s
 
 } 
 
-export async function createBracket(name: string, description: string, startDate: Date, endDate: Date) {
-    // Do some work and then add to the db
+export async function createBracket(user_id: bigint, name: string, description: string, startDate: Date, endDate: Date) {
+    // Do some error handling
     if (!name || !description || !startDate || !endDate) {
         console.error('Error failed to create bracket. One or more of the function parameters are undefined');
         return;
     }
+    if (name.length > 255) {
+        console.error('Error length of name must be less than or equal to 255 character');
+        return;
+    }
+    const current_day = new Date();
+    current_day.setHours(0, 0, 0, 0);
+    if (startDate < current_day) {
+        console.error('Error start date can not be in the past');
+        return;
+    }
+    if (isAheadAYear(startDate, endDate)) {
+        console.error('Error end date must be less than or equal to one year ahead of start date');
+        return;
+
+    }
+
+    // Change the date objects into the correct format to be added to the db
+    const sqlStartDate: string = startDate.toISOString().slice(0, 19).replace('T', ' ');
+    const sqlEndDate: string = endDate.toISOString().slice(0, 19).replace('T', ' ');
+
+    //Try and add bracket to the db
+    try {
+        await sql`BEGIN`;
+        await sql`
+            INSERT INTO brackets (name, description, start_date, end_date)
+            VALUES (${name}, ${description}, ${sqlStartDate}, ${sqlEndDate})
+        `;
+        await sql`
+            INSERT INTO user_brackets (user_id, bracket_id, role)
+            VALUES (${user_id.toString()}, (SELECT bracket_id FROM brackets WHERE name=${name}), 'super_admin')
+        `;
+        await sql`COMMIT`;
+
+    } catch (error) {
+        await sql`ROLLBACK`;
+        console.error('unexpected error while attempting to create a new bracket: ', error);
+    }
+
 }
 
